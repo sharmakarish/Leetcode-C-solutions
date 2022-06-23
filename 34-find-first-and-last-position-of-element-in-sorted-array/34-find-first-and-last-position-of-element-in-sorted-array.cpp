0fe9a3148e4612def14e8583fb5915110dd88717
Karishma Sharma <ghoshikarishma1@gmail.com>
2022-06-23 18:00:23 +0530

    Time: 23 ms (5.31%), Space: 13.7 MB (18.01%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,49 @@
+//optimized solution with binar search 
+class Solution {
+public:
+    int first_occurence( vector<int>& nums, int target){
+        int start =0, end = nums.size()-1;
+        int mid = start + (end -start)/2;
+        int ans = -1;
+        while(start<=end){
+            if(nums[mid] == target){
+                ans = mid;
+                end =mid-1;
+            }
+            else if(target> nums[mid]){ // traverse right
+                start = mid+1 ;
+            }
+              else if(target < nums[mid]){ // traverse left
+                 end = mid-1 ;
+            }
+            mid = start + (end -start)/2;
+        }
+        return ans;
+    }
+      int last_occurence( vector<int>& nums, int target){
+        int start =0, end = nums.size()-1;
+        int mid = start + (end -start)/2;
+        int ans = -1;
+        while(start<=end){
+            if(nums[mid] == target){
+                ans = mid;
+                start =mid+1;
+            }
+            else if(target > nums[mid]){ // traverse right
+                start = mid+1 ;
+            }
+              else if(target < nums[mid]){ // traverse right
+                 end = mid-1 ;
+            }
+            mid = start + (end -start)/2;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> occurence;
+        occurence.push_back(first_occurence(nums,target));
+        occurence.push_back(last_occurence(nums,target));
+        
+        return occurence;
+}
+};
