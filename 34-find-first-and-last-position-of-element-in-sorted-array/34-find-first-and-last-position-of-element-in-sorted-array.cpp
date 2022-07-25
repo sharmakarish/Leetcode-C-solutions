8373ad927f1f5c01455622a54eac9c25d78177ba
Karishma Sharma <ghoshikarishma1@gmail.com>
2022-07-25 16:59:08 +0530

    Time: 13 ms (48.91%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,6 +5,7 @@ public:
         int start =0, end = nums.size()-1;
         int mid = start + (end -start)/2;
         int ans = -1;
+        
         while(start<=end){
             if(nums[mid] == target){
                 ans = mid;
