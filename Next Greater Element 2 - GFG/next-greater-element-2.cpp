10290b3c9b40d4edf1d233dd9a4a07f4f5dee2a2
Karishma Sharma <ghoshikarishma1@gmail.com>
2023-05-05 09:07:15 +0530

    Added solution - LeetHub

---
+++ Next Greater Element 2 - GFG/next-greater-element-2.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> nextGreaterElement(int N, vector<int>& arr) {
+      vector < int > res(N, -1);
+      stack < int > st;
+      for (int i = 2 * N - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= arr[i % N]) {
+          st.pop();
+        }
+
+        if (i < N) {
+          if (!st.empty()) res[i] = st.top();
+        }
+        st.push(arr[i % N]);
+      }
+      return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) cin >> arr[i];
+
+        Solution obj;
+        vector<int> ans = obj.nextGreaterElement(N, arr);
+        for (auto &it : ans) {
+            cout << it << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
