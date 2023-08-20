aa8a72bfbb99a532a58c5b5fc09993c721de1479
Karishma Sharma <ghoshikarishma1@gmail.com>
2023-08-20 12:18:14 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,64 +10,19 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-		
-		
-	 int first_occurence(int arr[],int n, int x){
-        int start =0, end = n -1;
-        int mid = start + (end -start)/2;
-        int ans = -1;
-        
-        while(start<=end){
-            if(arr[mid] == x){
-                ans = mid;
-                end =mid-1;
-            }
-            else if(x> arr[mid]){ // traverse right
-                start = mid+1 ;
-            }
-              else if(x < arr[mid]){ // traverse left
-                 end = mid-1 ;
-            }
-            mid = start + (end -start)/2;
-        }
-        return ans;
-    }
-      int last_occurence(int arr[],int n, int x){
-        int start =0, end = n -1;
-        int mid = start + (end -start)/2;
-        int ans = -1;
-        while(start<=end){
-            if(arr[mid] == x){
-                ans = mid;
-                start =mid+1;
-            }
-            else if(x > arr[mid]){ // traverse right
-                start = mid+1 ;
-            }
-              else if(x < arr[mid]){ // traverse right
-                 end = mid-1 ;
-            }
-            mid = start + (end -start)/2;
-        }
-        return ans;
-    }
-    
 	int count(int arr[], int n, int x) {
-
-        int fo = (first_occurence(arr,n,x));
-        int lo = (last_occurence(arr,n,x));
-        
-        
-        if(fo == -1 || lo == -1)
-    {
-        return 0;
-    }
-    int ans = lo - fo + 1;
-    return ans;
-}
-
-
-
+	    unordemap<int,int> mp;
+	    for(int i = 0; i< n;i++){
+	        mp[arr[i]]++;
+	    }
+	    int ans = 0;
+	    for(auto it : mp){
+	        if(it.first == x){
+	            ans = it.second;
+	        }
+	    }
+	    return ans;
+	}
 };
 
 //{ Driver Code Starts.
