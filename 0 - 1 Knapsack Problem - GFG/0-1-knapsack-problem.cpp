df33fb4280b75e9631fc439e2a8aa1a352f6ca3c
Karishma Sharma <ghoshikarishma1@gmail.com>
2023-10-07 16:41:27 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -15,24 +15,26 @@ class Solution
 
     
     int knapSack(int w, int wt[], int val[], int n) 
-    {  // Create a 2D vector to store the dynamic programming table
-        vector<vector<int>> dp(n, vector<int>(w + 1, 0));
-        
+    {  
+        vector<int> prev(w+1 , 0) , cur(w +1, 0);
+
         // Initialize the first row (base case)
         for(int k = wt[0]; k <= w; k++) {
-            dp[0][k] = val[0];
+            prev[k] = val[0];
         }
     
         // Populate the dynamic programming table
         for(int ind = 1; ind < n; ind++) {
             for(int weight = 0; weight <= w; weight++) {
-                int nottake = dp[ind-1][weight];
-                int take = (wt[ind] <= weight) ? val[ind] + dp[ind-1][weight - wt[ind]] : INT_MIN;
-                dp[ind][weight] = max(take, nottake);
+                int nottake = prev[weight];
+                int take = (wt[ind] <= weight) ? val[ind] + prev[weight - wt[ind]] : INT_MIN;
+              
+               cur[weight] = max(take, nottake);
             }
+            prev = cur;
         }
         
-        return dp[n-1][w];
+        return prev[w];
     }
 };
 
