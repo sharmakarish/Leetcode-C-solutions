4f55020c7f7557a55aea040184b0f46e3340a3e6
Karishma Sharma <ghoshikarishma1@gmail.com>
2023-11-01 17:32:28 +0530

    Added solution - LeetHub

--- K-th element of two Arrays - GFG/kth-element-of-two-arrays.cpp
+++ K-th element of two Arrays - GFG/kth-element-of-two-arrays.cpp
@@ -8,22 +8,51 @@ class Solution{
     public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k)
     {
-        vector<int> res;
-        for(int i = 0; i < n; i++){
-            res.push_back(arr1[i]);
+       
+       int i = 0;
+       int j = 0;
+       int count = 0;
+
+// Dont even have to merge , just iterate one by one and return 
+
+// if (count == k) then return that element
+
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                count++;
+                if (count == k) {
+                    return arr1[i];
+                }
+                i++;
+            } else {
+                count++;
+                if (count == k) {
+                    return arr2[j];
+                }
+                j++;
+            }
         }
-         for(int i = 0; i < m; i++){
-            res.push_back(arr2[i]);
+
+        // If we haven't found the kth element yet, we need to handle it here.
+        while (i < n) {
+            count++;
+            if (count == k) {
+                return arr1[i];
+            }
+            i++;
         }
-        sort(res.begin(), res.end());
-        int ans = 0;
-        for(int i = 0; i < res.size(); i ++){
-            if(i == k-1){
-                ans = res[i];
-                break;
+
+        while (j < m) {
+            count++;
+            if (count == k) {
+                return arr2[j];
+            }
+            j++;
         }
-        }
-        return ans;
+
+        return -1; // Return -1 if k is out of bounds.
+        
+        
     }
 };
 
